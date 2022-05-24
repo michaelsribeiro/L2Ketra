f8290aa14ea3ac915c436cdae72cd8dcff3af2e8
Michael Ribeiro <m.ribeiroabd@gmail.com>
2022-05-23 23:08:08 -0300

    .

---
+++ index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>L2 Ketra - Interlude</title>
+
+    <!-- FAVICON -->
+    <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+<body>
+    <main>
+        <div class="container">
+            <div class="serverStatus">
+                <span></span>
+                <p>Server Online</p>
+                <div class="players_on">739</div>
+            </div>
+            <nav></nav>
+        </div>
+    </main>
+    <section class="statsbox">
+        <div class="container stats">
+            <div class="results">
+                <div class="box-rank">
+                    <div class="title">
+                        <span class="pvp"></span>
+                        <div class="desc">Top Pvp</div>
+                        <a href="http://#">view more »</a>
+                    </div>
+                    <div class="rank">
+                        <div class="p1 pos">1</div>
+                        <div class="nickname">Administrador</div>
+                        <div class="total">8230</div>
+                    </div>
+                    <div class="rank">
+                        <div class="p2 pos">2</div>
+                        <div class="nickname">Administrador</div>
+                        <div class="total">8230</div>
+                    </div>
+                    <div class="rank">
+                        <div class="p3 pos">3</div>
+                        <div class="nickname">Administrador</div>
+                        <div class="total">8230</div>
+                    </div>
+                </div>
+                <div class="box-rank">
+                    <div class="title">
+                        <span class="pk"></span>
+                        <div class="desc">Top Pk</div>
+                        <a href="http://#">view more »</a>
+                    </div>
+                    <div class="rank">
+                        <div class="p1 pos">1</div>
+                        <div class="nickname">Administrador</div>
+                        <div class="total">8230</div>
+                    </div>
+                    <div class="rank">
+                        <div class="p2 pos">2</div>
+                        <div class="nickname">Administrador</div>
+                        <div class="total">8230</div>
+                    </div>
+                    <div class="rank">
+                        <div class="p3 pos">3</div>
+                        <div class="nickname">Administrador</div>
+                        <div class="total">8230</div>
+                    </div>
+                </div>
+                <div class="box-rank">
+                    <div class="title">
+                        <span class="clan"></span>
+                        <div class="desc">Top Clan</div>
+                        <a href="http://#">view more »</a>
+                    </div>
+                    <div class="rank">
+                        <div class="p1 pos">1</div>
+                        <div class="nickname">Administrador</div>
+                        <div class="total">8230</div>
+                    </div>
+                    <div class="rank">
+                        <div class="p2 pos">2</div>
+                            <div class="nickname">Administrador</div>
+                            <div class="total">8230</div>
+                    </div>
+                    <div class="rank">
+                        <div class="p3 pos">3</div>
+                        <div class="nickname">Administrador</div>
+                        <div class="total">8230</div>
+                    </div>
+                </div>
+                <div class="box-rank">
+                    <div class="title">
+                        <span class="online"></span>
+                        <div class="desc">Top Online</div>
+                        <a href="http://#">view more »</a>
+                    </div>
+                    <div class="rank">
+                        <div class="p1 pos">1</div>
+                        <div class="nickname">Administrador</div>
+                        <div class="total">8230</div>
+                    </div>
+                    <div class="rank">
+                        <div class="p2 pos">2</div>
+                        <div class="nickname">Administrador</div>
+                        <div class="total">8230</div>
+                    </div>
+                    <div class="rank">
+                        <div class="p3 pos">3</div>
+                        <div class="nickname">Administrador</div>
+                        <div class="total">8230</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container page">
+            <article>
+                <a href=""><img src="assets/images/main-banner.jpg" alt="Register Now"></a>
+                <hr>
+                <div class="title">
+                    <h1>News</h1>
+                    <a href="#">View More News »</a>
+                </div>
+                <div class="news">
+                    <div class="new">
+                        <a href="#">
+                            <img src="assets/images/1.jpg">
+                        </a>
+                    </div>
+                    <div class="new">
+                        <a href="#">
+                            <img src="assets/images/2.jpg">
+                        </a>
+                    </div>
+                    <div class="new">
+                        <a href="#">
+                            <img src="assets/images/3.jpg">
+                        </a>
+                    </div>
+                    <div class="new">
+                        <a href="#">
+                            <img src="assets/images/1.jpg">
+                        </a>
+                    </div>
+                </div>
+            </article>
+            <aside>
+                <div class="box">
+                    <div class="text"><h3>Donations</h3></div>
+                    <a href="#"><img src="assets/images/donateb_EN.jpg" alt="Make a Donation"></a>
+                </div>
+                <div class="box">
+                    <div class="text"><h3>Gallery</h3></div>
+                    <a href="#"><img src="assets/images/donateb_EN.jpg" alt="Make a Donation"></a>
+                </div>
+                <div class="box">
+                    <div class="text"><h3>Facebook</h3></div>
+                    <div class="fb-page" data-href="https://www.facebook.com/l2ketrainterlude" data-tabs="" data-width="308px" data-height="" data-small-header="false" data-adapt-container-width="false" data-hide-cover="false" data-show-facepile="false">
+                        <blockquote cite="https://www.facebook.com/l2ketrainterlude" class="fb-xfbml-parse-ignore">
+                            <a href="https://www.facebook.com/l2ketrainterlude">Lineage 2 Ketra</a>
+                        </blockquote>
+                    </div>
+                </div>
+            </aside>
+        </div>
+    </section>
+    <footer>
+        <div class="container">
+            <div class="links">
+                <div class="account">
+                    <ul class="navfooter">
+                        <h2>Account</h2>
+                        <li><a href="#">Register</a></li>
+                        <li><a href="#">Change Password</a></li>
+                        <li><a href="#">Recover Account</a></li>
+                    </ul>
+                </div>
+                <div class="server">
+                    <ul class="navfooter">
+                        <h2>Server</h2>
+                        <li><a href="#">Downloads</a></li>
+                        <li><a href="#">Information</a></li>
+                        <li><a href="#">Support</a></li>
+                        <li><a href="#">Rules</a></li>
+                        <li><a href="#">Donation</a></li>
+                        <li><a href="#">Gallery</a></li>
+                    </ul>
+                </div>
+                <div class="info">
+                    <ul class="navfooter">
+                        <h2>Info</h2>
+                        <li><a href="#">Cookies</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">User Agreement</a></li>
+                        <li><a href="#">License</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="rights">
+                <div class="logo"></div>
+                <div class="copy">© 2022 L2KETRA - ALL RIGHTS RESERVED</div>
+                <p>LINEAGE II AND ALL ASSOCIATED LOGOS AND DESIGNS ARE TRADEMARKS OR REGISTERED <br>TRADEMARKS OF NCSOFT CORPORATION. 
+                    ALL OTHER TRADEMARKS OR REGISTERED TRADEMARKS<br> ARE PROPERTY OF THEIR RESPECTIVE OWNERS.</p> 
+            </div>
+        </div>
+    </footer>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v13.0" nonce="0SrYOoyX"></script>
+</body>
+</html>
