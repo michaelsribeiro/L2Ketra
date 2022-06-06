57293a9ffa600d4889d1f360a909eeb43a7d1bc6
Michael Ribeiro <m.ribeiroabd@gmail.com>
2022-06-05 23:53:11 -0300

    Download page

--- pages/download.php
+++ pages/download.php
@@ -1,7 +1,62 @@
 <div class="download">
+    <h1>Downloads</h1>
     <div class="option">
         <h2>Download Server Files</h2>
-        <a class="btn-default" href="">PATCH FULL [MEGA]</a>
+        <a class="btn-default first" href="">PATCH FULL [MEGA]</a>
         <a class="btn-default" href="">PATCH FULL [MEDIAFIRE]</a>
+    </div>
+    <div class="option">
+        <h2>Download Interlude Client</h2>
+        <a class="btn-default first" href="https://mega.nz/file/v5h2hbxK#_KMTiu-VRE_GciMMuI9SPAMpFQDhri-LIVvXN2qMYZ0" target="blank">FROM MEGA</a>
+        <a class="btn-default" href="https://www.mediafire.com/file/3ky7f3mfp2fgcqk/Lineage2_Interlude.exe/file" target="blank">FROM MEDIAFIRE</a>
+    </div>
+    <div class="option">
+        <h2>Requirements</h2>
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">Setup</th>
+                <th scope="col">Minimum</th>
+                <th scope="col">Recomended</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="row-dark">
+                    <td>Processor</td>
+                    <td>Dual Core</td>
+                    <td>Intel Core i3</td>
+                </tr>
+                <tr>
+                    <td>Memory</td>
+                    <td>2 GB</td>
+                    <td>4 GB</td>
+                </tr>
+                <tr class="row-dark">
+                    <td>Video Graphics</td>
+                    <td>Graphics (integrated)</td>
+                    <td>Nvidia or AMD</td>
+                </tr>
+                <tr>
+                    <td>Display</td>
+                    <td>Any</td>
+                    <td>Any</td>
+                </tr>
+                <tr class="row-dark">
+                    <td>System</td>
+                    <td>Windows 7</td>
+                    <td>Windows 10</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="option">
+        <div class="instructions">
+            <h3>How to install? It's very simple!</h3>
+            <p>1 - Download the Interlude Client and Full Patch</p>
+            <p>2 - Install Interlude Client</p>
+            <p>3 - Extract the Full Patch in Interlude Cliente folder</p>
+            <p>4 - Open the game in Client Lineage II/system/L2.exe</p>
+            <p>5 - Create a shortcut icon in your Desktop</p>
+        </div>
     </div>
 </div>
