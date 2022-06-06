0d812f0d4a94fca86285ea5ac93b611257fa84eb
Michael Ribeiro <m.ribeiroabd@gmail.com>
2022-06-05 22:42:26 -0300

    Change pass and Recover account pages

---
+++ pages/download.php
@@ -0,0 +1,7 @@
+<div class="download">
+    <div class="option">
+        <h2>Download Server Files</h2>
+        <a class="btn-default" href="">PATCH FULL [MEGA]</a>
+        <a class="btn-default" href="">PATCH FULL [MEDIAFIRE]</a>
+    </div>
+</div>
