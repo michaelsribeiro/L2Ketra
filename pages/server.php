09098fcf3888e20c47ee84d685e739861d9e05a4
Michael Ribeiro <m.ribeiroabd@gmail.com>
2022-06-07 10:55:34 -0300

    Server informations page finished

---
+++ pages/server.php
@@ -0,0 +1,336 @@
+<div class="server-info">
+    <div class="server-info-column">
+        <div class="info-box">
+            <div class="info-title">
+                <h1 class="title">Informations</h1>
+                <span class="subtitle">All informations from Server</span>
+            </div>
+            <div class="info-header">
+                <div class="box-big">
+                    <span>Rates</span>
+                    <span>x500</span>
+                </div>
+                <div class="box-big">
+                    <span>Enchant</span>
+                    <span>+5 » +25</span>
+                </div>
+                <div class="box-big">
+                    <span>Time</span>
+                    <span>GMT-3</span>
+                </div>
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="assets/images/subclass.png">
+                </div>
+                <span class="desc">Subclass</span>
+                <span class="arrow">»</span>
+                <span class="text">Free</span>
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="assets/images/nobless.png">
+                </div>
+                <span class="desc">Nobless</span>
+                <span class="arrow">»</span>
+                <span class="text">Kill Barakiel or Quest</span>
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="assets/images/argument.png">
+                </div>
+                <span class="desc">Arguments</span>
+                <span class="arrow">»</span>
+                <span class="text">5% chance obtain skill</span>
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="assets/images/skill1427.png">
+                </div>
+                <span class="desc">Skill FOI</span>
+                <span class="arrow">»</span>
+                <span class="text">Style H5</span>
+            </div>
+        </div>
+        <div class="info-box">
+            <div class="info-title">
+                <h1 class="title">Scrolls Enchants</h1>
+                <span class="subtitle">Weapons and Armors Enchants</span>
+            </div>
+            <div class="box">
+                <span class="desc">Safe Enchant</span>
+                <span class="arrow">»</span>
+                <span class="text">+5</span>
+            </div>
+            <div class="box">
+                <span class="desc">Max Enchant</span>
+                <span class="arrow">»</span>
+                <span class="text">+25</span>
+            </div>
+            <div class="box">
+                <div>
+                    <span class="desc">Scroll Enchant │ Min +5 • Max +20</span>
+                    <span class="text">to +5(100% Chance) │ +6 to +18(70% Chance)</span>
+                </div>
+            </div>         
+            <div class="box">
+                <div>
+                    <span class="desc">Blessed Enchant │ Min +5 • Max +18 │ Fail? Return +5</span>
+                    <span class="text">to +5(100% Chance) │ +6 to +20(80% Chance)</span>
+                </div>
+            </div>         
+            <div class="box">
+                <div>
+                    <span class="desc">Crystal Enchant │ Min +5 • Max +25 │ Fail? Return +20</span>
+                    <span class="text">to +20(100%) │ +20 to +25(60%) • Decress 10% per enchant</span>
+                </div>
+            </div>         
+        </div>
+        <div class="info-box">
+            <div class="info-title">
+                <h1 class="title">Buffs and Power</h1>
+                <span class="subtitle">Magic Buffs Information</span>
+            </div>
+            <div class="box">
+                <span class="desc">Buff Slots</span>
+                <span class="arrow">»</span>
+                <span class="text">32+4</span>
+            </div>
+            <div class="box">
+                <span class="desc">Buff Time</span>
+                <span class="arrow">»</span>
+                <span class="text">2 hours</span>
+            </div>                 
+            <div class="box">
+                <span class="desc">Scheme Buffs</span>
+                <span class="arrow">»</span>
+                <span class="text">Active</span>
+            </div>                 
+            <div class="box">
+                <span class="desc">All Buffs in NPC</span>
+                <span class="arrow">»</span>
+                <span class="text">Active</span>
+            </div>                 
+        </div>
+        <div class="info-box">
+            <div class="info-title">
+                <h1 class="title">Customs</h1>
+                <span class="subtitle">Armors</span>
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="assets/images/titanium.png">
+                </div>
+                <span class="desc">Titanium Armor</span>
+                <span class="arrow">»</span>
+                <span class="text">Stage 1</span>
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="assets/images/vesper.webp">
+                </div>
+                <span class="desc">Vesper</span>
+                <span class="arrow">»</span>
+                <span class="text">Stage 1</span>
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="assets/images/dynasty.png">
+                </div>
+                <span class="desc">Dynasty</span>
+                <span class="arrow">»</span>
+                <span class="text">Stage 1</span>
+            </div>           
+        </div>
+        <div class="info-box">
+            <div class="info-title">
+                <h1 class="title">Customs</h1>
+                <span class="subtitle">Weapons</span>
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="assets/images/s-grade.png">
+                </div>
+                <span class="desc">S-Grade</span>
+                <span class="arrow">»</span>
+                <span class="text">Stage 1</span>
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="assets/images/epic.png">
+                </div>
+                <span class="desc">Epic</span>
+                <span class="arrow">»</span>
+                <span class="text">Stage 1</span>
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="assets/images/weapon-dynasty.png">
+                </div>
+                <span class="desc">Dynasty</span>
+                <span class="arrow">»</span>
+                <span class="text">Stage 1</span>
+            </div>           
+            <div class="box">
+                <div class="image">
+                    <img src="assets/images/icarus.png">
+                </div>
+                <span class="desc">Icarus</span>
+                <span class="arrow">»</span>
+                <span class="text">Stage 1</span>
+            </div>           
+        </div>
+    </div>
+    <div class="server-info-column">
+        <div class="info-box">
+            <div class="info-title">
+                <h1 class="title">General</h1>
+                <span class="subtitle">Grand Boss Informations</span>
+            </div>
+            <div class="info-header">
+                <div class="box-big">
+                    <span>Zone</span>
+                    <span>Chaotic</span>
+                </div>
+                <div class="box-big">
+                    <span>Boss Level</span>
+                    <span>80</span>
+                </div>
+                <div class="box-big">
+                    <span>Informations</span>
+                    <span>.raidinfo</span>
+                </div>
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="assets/images/core.png">
+                </div>
+                <div>
+                    <span class="desc">Core</span>
+                    <span class="text">Everyday <strong>17:00(GMT-3)</strong></span>
+                </div>
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="assets/images/orfen.png">
+                </div>
+                <div>
+                    <span class="desc">Orfen</span>
+                    <span class="text">Everyday <strong>18:00(GMT-3)</strong></span>
+                </div>
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="assets/images/zaken.png">
+                </div>
+                <div>
+                    <span class="desc">Zaken</span>
+                    <span class="text">Everyday <strong>19:00(GMT-3)</strong></span>
+                </div>
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="assets/images/queen_ant.png">
+                </div>
+                <div>
+                    <span class="desc">Queen Ant</span>
+                    <span class="text">Everyday <strong>20:00(GMT-3)</strong></span>
+                </div>
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="assets/images/baium.png">
+                </div>
+                <div>
+                    <span class="desc">Baium</span>
+                    <span class="text">Every 2 days <strong>21:00(GMT-3)</strong></span>
+                </div>
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="assets/images/frintezza.png">
+                </div>
+                <div>
+                    <span class="desc">Frintezza</span>
+                    <span class="text">Every 2 days <strong>21:00(GMT-3)</strong></span>
+                </div>
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="assets/images/antharas.png">
+                </div>
+                <div>
+                    <span class="desc">Antharas</span>
+                    <span class="text">Every 3 days <strong>22:00(GMT-3)</strong></span>
+                </div>
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="assets/images/valakas.png">
+                </div>
+                <div>
+                    <span class="desc">Valakas</span>
+                    <span class="text">Every 3 days <strong>22:00(GMT-3)</strong></span>
+                </div>
+            </div>
+        </div>
+        <div class="info-box">
+            <div class="info-title">
+                <h1 class="title">Olympiad</h1>
+                <span class="subtitle">Heroes Information</span>
+            </div>            
+            <div class="box">
+                <span class="desc">Period</span>
+                <span class="arrow">»</span>
+                <span class="text">7 Days</span>
+            </div>           
+            <div class="box">
+                <span class="desc">Time</span>
+                <span class="arrow">»</span>
+                <span class="text">18:00 ~ 00:00 GMT-3</span>
+            </div>           
+            <div class="box">
+                <span class="desc">Class vs Class fights</span>
+                <span class="arrow">»</span>
+                <span class="text">Disabled</span>
+            </div>           
+            <div class="box">
+                <span class="desc">Items</span>
+                <span class="arrow">»</span>
+                <span class="text">no-Custom +8</span>
+            </div>           
+            <div class="box">
+                <span class="desc">Jewels</span>
+                <span class="arrow">»</span>
+                <span class="text">Only Frintezza Allowed</span>
+            </div>           
+        </div>
+        <div class="info-box">
+            <div class="info-title">
+                <h1 class="title">Server Protections</h1>
+                <span class="subtitle">Security Informations</span>
+            </div>            
+            <div class="box">
+                <span class="desc">Farm</span>
+                <span class="arrow">»</span>
+                <span class="text">1 Screen per PC</span>
+            </div>           
+            <div class="box">
+                <span class="desc">All</span>
+                <span class="arrow">»</span>
+                <span class="text">2 Screen per PC</span>
+            </div>           
+            <div class="box">
+                <span class="desc">DDOS Protection</span>
+                <span class="arrow">»</span>
+                <span class="text">Epic</span>
+            </div>           
+            <div class="box">
+                <span class="desc">AntiZerg</span>
+                <span class="arrow">»</span>
+                <span class="text">27 Players per Clan</span>
+            </div>          
+        </div>
+    </div>
+    
+</div>
