38bde7f199649df0795711f489da149e535e17dc
Michael Ribeiro <m.ribeiroabd@gmail.com>
2022-06-08 19:09:47 -0300

    Castle Siege page finished

--- engine/db_connect.php
+++ engine/db_connect.php
@@ -1,9 +1,14 @@
 <?php
 
-    $servername = 'localhost';
-    $username = 'root';
-    $password = '';
-    $dbname = 'lineage2db';
+    //$servername = 'localhost';
+    //$username = 'root';
+    //$password = '';
+    //$dbname = 'lineage2db';
+
+    $servername = 'us-cdbr-east-05.cleardb.net';
+    $username = 'b2d1def57a4be5';
+    $password = '03a148a9';
+    $dbname = 'heroku_64fa31b55003450';
 
     $conn = mysqli_connect($servername, $username, $password, $dbname);
 
