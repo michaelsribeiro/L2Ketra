8bc652929b40a266968b42a9f6d5e00dcf96018c
Michael Ribeiro <m.ribeiroabd@gmail.com>
2022-06-08 23:21:16 -0300

    Donate page

---
+++ pages/donation.php
@@ -0,0 +1,62 @@
+<?php ?>
+<div class="donate">
+    <h1>Donate</h1>
+    <table cellspacing="0" cellpadding="0" border="0" class="table">
+        <thead>
+            <tr>
+            <th scope="col">ITEM</th>
+            <th scope="col">PRICE</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1 Ticket Donate</td>
+                <td>1,00 BRL</td>
+            </tr>
+            <tr class="row-dark">
+                <td>Noblesse Status</td>
+                <td>5 Ticket Donate</td>
+            </tr>
+            <tr>
+                <td>Aio Tattoo</td>
+                <td>20 Ticket Donate</td>
+            </tr>
+            <tr class="row-dark">
+                <td>Acessory +10%</td>
+                <td>20 Ticket Donate</td>
+            </tr>
+            <tr>
+                <td>Vip 30 Days</td>
+                <td>50 Ticket Donate</td>
+            </tr>
+        </tbody>
+    </table>
+    <div class="donate-info">
+        <h1>Vip System</h1>
+        <table cellspacing="0" cellpadding="0" border="0" class="table">
+        <thead>
+            <tr>
+            <th scope="col">DESCRIPTION</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>*Farm Drop 2x</td>
+            </tr>
+            <tr class="row-dark">
+                <td>*Event Drop 2x (Tournament / TvT)</td>
+            </tr>
+            <tr>
+                <td>*Exclusive Skins</td>
+            </tr>
+            <tr class="row-dark">
+                <td>*Exclusive Color Name Manager</td>
+            </tr>
+            <tr>
+                <td>*Exclusive Farm Zones</td>
+            </tr>
+        </tbody>
+        </table>
+        <a href="#" class="btn-default">Donate Panel</a>
+    </div>
+</div>
