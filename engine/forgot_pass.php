4890507c6add6f02bfbdc49e1ca433d46fe7dca2
Michael Ribeiro <m.ribeiroabd@gmail.com>
2022-06-15 21:54:06 -0300

    <feat> Recover password

--- engine/forgot_pass.php
+++ engine/forgot_pass.php
@@ -35,7 +35,7 @@ if(isset($_POST['email'])) {
 }
 
 function sendEmail($email, $key) {
-    $from = 'michael-abd@live.com';
+    /*$from = 'michael-abd@live.com';
     $to = $email;
     $subject = 'L2 Ketra | Redefinição de Senha';
     $message = 'Email de teste';
@@ -55,6 +55,33 @@ function sendEmail($email, $key) {
         $_SESSION['error'] = "Este campo é obrigatório";
         header("Location: ../?pages=forgot");
         exit;
+    }*/
+
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $_ENV['l2-ketra.herokuapp.com'] . "/api/i/v1/email",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS =>"{\"recipients\":[{\"email\":\"contato@lojaphoneshop.com.br\"}],\"title\":\"Title\",\"html\":\"Body\"}",
+        CURLOPT_HTTPHEADER => array(
+            "x-trustifi-key: " . $_ENV['fff7f662051f68935dbf6fed0c4d45189460f5c49ec0ba18'],
+            "x-trustifi-secret: " . $_ENV['0a3bd21e016f6d1b3fd6961f949534f4'],
+            "content-type: application/json"
+        )
+    ));
+
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    curl_close($curl);
+    if ($err) {
+        echo "cURL Error #:" . $err;
+    } else {
+        echo $response;
     }
 
 }
